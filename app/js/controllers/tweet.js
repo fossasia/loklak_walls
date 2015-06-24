5ab052524e1f51f825df3b8ff8422c53c4db05aa
Prashant Kiran <prasht63@gmail.com>
2015-06-25 02:03:44 +0530

    Added support for geoLocation coordinates, Implemented modal hide on submit

--- app/js/controllers/tweet.js
+++ app/js/controllers/tweet.js
@@ -7,15 +7,35 @@ var hello=require('../components/hello.all');
 controllersModule.controller('HomeCtrl', ['$rootScope', function($rootScope) {
 
     $rootScope.root.tweet="";
-    $rootScope.root.foo = function() 
-    {    
+    $rootScope.root.location={};
+    var locationURL;
+    $rootScope.root.postTweet = function() 
+    { 
+    $('#myModal').modal('hide');     
     var message = $rootScope.root.tweet;
     var tweet = encodeURIComponent(message);
+    
     console.log(message);
     hello('twitter').api('me/share', 'POST', {
         message : tweet
     });
     
     };
+    $rootScope.root.getLocation = function(){
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(setPosition);
+    } else { 
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function setPosition(position) {
+    $rootScope.root.location.latitude=position.coords.latitude; 
+    $rootScope.root.location.longitude=position.coords.longitude;
+    $rootScope.root.location.tile="http://loklak.org/vis/map.png?text=Test&mlat="+position.coords.latitude+"&mlon="+position.coords.longitude+"&zoom=13&width=512&height=256";
+    $("#locationtile").attr("src", $rootScope.root.location.tile);
+    $("#locationtile").removeClass('hidden');
+    console.log( $rootScope.root.location.tile);
+}
 
 }]);
